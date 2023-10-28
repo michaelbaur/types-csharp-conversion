0d128f0da4602485dfd75d3c542e878535bd1631
Michael Baur <michael.baur@yworks.com>
2023-10-28 17:09:41 +0200

    Add a dotnet solution and project

--- src/converter.ts
+++ src/converter.ts
@@ -1,33 +1,12 @@
-import ts, {
-  forEachChild,
-  Identifier,
-  InterfaceDeclaration,
-  isArrayTypeNode,
-  isIdentifier,
-  isInterfaceDeclaration,
-  isMethodSignature,
-  isPropertySignature,
-  isReadonlyKeywordOrPlusOrMinusToken,
-  isTypeReferenceNode,
-  MethodSignature,
-  Modifier,
-  NodeArray,
-  ParameterDeclaration,
-  PropertySignature,
-  SourceFile,
-  StringLiteral,
-  SyntaxKind,
-  TypeNode,
-} from 'typescript'
+import ts from 'typescript'
 
-export const preamble = `// ReSharper disable InconsistentNaming, RedundantExtendsListEntry
+const disableWarnings = `// ReSharper disable InconsistentNaming, RedundantExtendsListEntry, TypeParameterCanBeVariant
 #pragma warning disable CS0109 // Member does not hide an inherited member; new keyword is not required
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-
 `
 
 type Context = {
-  sourceFile: SourceFile
+  sourceFile: ts.SourceFile
 }
 
 const reservedNames: Record<string, boolean> = {
@@ -42,29 +21,31 @@ const reservedNames: Record<string, boolean> = {
   operator: true,
   public: true,
   switch: true,
+  static: true,
+  private: true,
   this: true,
 }
 
-function getTypeName(type: TypeNode, context: Context): string {
+function getTypeName(type: ts.TypeNode, context: Context): string {
   switch (type.kind) {
-    case SyntaxKind.StringKeyword:
+    case ts.SyntaxKind.StringKeyword:
       return 'string'
-    case SyntaxKind.NumberKeyword:
+    case ts.SyntaxKind.NumberKeyword:
       return 'double'
-    case SyntaxKind.BooleanKeyword:
+    case ts.SyntaxKind.BooleanKeyword:
       return 'bool'
-    case SyntaxKind.AnyKeyword:
+    case ts.SyntaxKind.AnyKeyword:
       return 'object'
-    case SyntaxKind.VoidKeyword:
+    case ts.SyntaxKind.VoidKeyword:
       return 'void'
-    case SyntaxKind.SymbolKeyword:
+    case ts.SyntaxKind.SymbolKeyword:
       return 'Symbol'
   }
 
-  if (isTypeReferenceNode(type)) {
-    return (type.typeName as Identifier).text
+  if (ts.isTypeReferenceNode(type)) {
+    return (type.typeName as ts.Identifier).text
   }
-  if (isArrayTypeNode(type)) {
+  if (ts.isArrayTypeNode(type)) {
     return getTypeName(type.elementType, context) + '[]'
   }
 
@@ -75,40 +56,50 @@ function getTypeName(type: TypeNode, context: Context): string {
 }
 
 function getSafeName(
-  member: PropertySignature | MethodSignature | Identifier,
+  member: ts.PropertySignature | ts.MethodSignature | ts.Identifier,
 ): string {
-  const name = isIdentifier(member)
+  const name = ts.isIdentifier(member)
     ? member.text
-    : (member.name as StringLiteral).text
+    : (member.name as ts.StringLiteral).text
   // TODO Improve writing of reserved member names
   return !reservedNames[name]
     ? name
-    : isIdentifier(member)
-      ? `${name}Parameter`
-      : isPropertySignature(member)
-        ? `${name}Property`
-        : `${name}Method`
+    : ts.isIdentifier(member)
+    ? `${name}Parameter`
+    : ts.isPropertySignature(member)
+    ? `${name}Property`
+    : `${name}Method`
 }
 
-function isReadonly(modifiers: NodeArray<Modifier> | undefined): boolean {
-  return modifiers?.some((m) => isReadonlyKeywordOrPlusOrMinusToken(m)) ?? false
+function isReadonly(modifiers: ts.NodeArray<ts.Modifier> | undefined): boolean {
+  return (
+    modifiers?.some((m) => ts.isReadonlyKeywordOrPlusOrMinusToken(m)) ?? false
+  )
 }
 
 function getParameterList(
-  parameterDeclaration: NodeArray<ParameterDeclaration>,
+  parameterDeclaration: ts.NodeArray<ts.ParameterDeclaration>,
   context: Context,
 ) {
   return parameterDeclaration
     .map(
       (para) =>
         `${getTypeName(para.type!, context)} ${getSafeName(
-          para.name as Identifier,
+          para.name as ts.Identifier,
         )}`,
     )
     .join(', ')
 }
 
-function getBaseTypes(node: InterfaceDeclaration): string {
+function getTypeParameters(node: ts.InterfaceDeclaration): string {
+  if (!node.typeParameters?.length) {
+    return ''
+  }
+  const parameters = node.typeParameters.map((parameter) => parameter.name.text)
+  return `<${parameters.join(', ')}>`
+}
+
+function getBaseTypes(node: ts.InterfaceDeclaration): string {
   if (!node.heritageClauses) {
     return ''
   }
@@ -116,14 +107,14 @@ function getBaseTypes(node: InterfaceDeclaration): string {
   const typeNames = node.heritageClauses[0].types.map(
     // TODO Support generics
     // TODO Call getTypeName
-    (value) => (value.expression as Identifier).text,
+    (value) => (value.expression as ts.Identifier).text,
   )
   return `: ${typeNames.join(', ')} `
 }
 
-function getMembers(node: InterfaceDeclaration, context: Context): string[] {
+function getMembers(node: ts.InterfaceDeclaration, context: Context): string[] {
   return node.members.map((member) => {
-    if (isPropertySignature(member)) {
+    if (ts.isPropertySignature(member)) {
       // TODO support modifier, especially readonly
       const name = getSafeName(member)
       const type = getTypeName(member.type!, context)
@@ -131,8 +122,8 @@ function getMembers(node: InterfaceDeclaration, context: Context): string[] {
       const accessors = 'get;' + (!readonly ? ' set;' : '')
       return `public new ${type} ${name} { ${accessors} }`
     }
-    if (isMethodSignature(member)) {
-      const ms = member as MethodSignature
+    if (ts.isMethodSignature(member)) {
+      const ms = member as ts.MethodSignature
       const name = getSafeName(member)
       const type = getTypeName(member.type!, context)
       const parameters = getParameterList(ms.parameters, context)
@@ -142,9 +133,29 @@ function getMembers(node: InterfaceDeclaration, context: Context): string[] {
   })
 }
 
-export function getFileContent(sourceFile: SourceFile): string[] {
+export function getNamespace(filename: string) {
+  return filename.includes('dom')
+    ? 'DOM'
+    : filename.includes('webworker')
+    ? 'Worker'
+    : filename.includes('decorators')
+    ? 'Decorators'
+    : 'Lib'
+}
+
+export function getFileContent(filename: string): string[] {
+  const program = ts.createProgram([filename], { allowJs: true })
+  const sourceFile = program.getSourceFile(filename)!
+
   const context = { sourceFile }
-  const interfaces: string[] = []
+  const namespace = getNamespace(filename)
+
+  // TODO Parse types into usings
+
+  const interfaces: string[] = [
+    disableWarnings,
+    `namespace ECMAScript.${namespace};`,
+  ]
 
   for (const match of sourceFile.text.matchAll(
     /\/\/\/\s*<reference\s+lib="(?<libName>.*)"\s*\/>/g,
@@ -152,17 +163,18 @@ export function getFileContent(sourceFile: SourceFile): string[] {
     interfaces.push(`// Referenced lib: ${match.groups!.libName}`)
   }
 
-  forEachChild(sourceFile, (node) => {
-    if (isInterfaceDeclaration(node)) {
+  ts.forEachChild(sourceFile, (node) => {
+    if (ts.isInterfaceDeclaration(node)) {
       const name = node.name.text
       // TODO Support generics
+      const typeParameters = getTypeParameters(node)
       const baseTypes = getBaseTypes(node)
       const members = getMembers(node, context)
       const typeContent =
         members.length === 0 ? '' : `\n  ${members.join('\n  ')}\n`
 
       interfaces.push(
-        `public partial interface ${name} ${baseTypes} {${typeContent}}`,
+        `public partial interface ${name}${typeParameters} ${baseTypes} {${typeContent}}`,
       )
     }
   })
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,39 @@
 import fg from 'fast-glob'
 import fs from 'fs-extra'
 import * as path from 'node:path'
-import ts from 'typescript'
-import { getFileContent, preamble } from './converter.js'
+import { getFileContent, getNamespace } from './converter.js'
 
 const toolName = 'Types-C#-Conversion'
 
 function convertFile(filename: string, targetDir: string): void {
-  const program = ts.createProgram([filename], { allowJs: true })
-  const sourceFile = program.getSourceFile(filename)!
+  const namespace = getNamespace(filename)
   const targetFile = path.join(
-    targetDir,
+    path.join(targetDir, namespace),
     `${path.basename(filename, `.d.ts`)}.cs`,
   )
 
-  const types = getFileContent(sourceFile)
+  const types = getFileContent(filename)
   console.log(`[${toolName}] Converted ${filename}`)
 
   const pagination = 300
   if (types.length < pagination) {
-    fs.writeFileSync(targetFile, preamble + types.join('\n\n'))
+    fs.writeFileSync(targetFile, types.join('\n\n'))
     return
   }
   for (let i = 0; i * pagination < types.length; i++) {
     fs.writeFileSync(
       targetFile.replace('.cs', `.p${i}.cs`),
-      preamble + types.slice(i * pagination, (i + 1) * pagination).join('\n\n'),
+      types.slice(i * pagination, (i + 1) * pagination).join('\n\n'),
     )
   }
 }
 
 async function convertDir(dirName: string, targetDir: string): Promise<void> {
-  fs.emptyDirSync(targetDir)
+  fs.emptyDirSync(path.join(targetDir, 'DOM'))
+  fs.emptyDirSync(path.join(targetDir, 'Decorators'))
+  fs.emptyDirSync(path.join(targetDir, 'Lib'))
+  fs.emptyDirSync(path.join(targetDir, 'Worker'))
+
   const typeFiles = await fg([`${dirName}/lib.*.d.ts`], {
     ignore: [
       // The following files just reference other libs
@@ -39,17 +41,17 @@ async function convertDir(dirName: string, targetDir: string): Promise<void> {
       '**/lib.d.ts',
       '**/lib.es6.d.ts',
       '**/lib.esnext.d.ts',
-      '**/lib.2015.d.ts',
-      '**/lib.2016.d.ts',
-      '**/lib.2017.d.ts',
-      '**/lib.2018.d.ts',
-      '**/lib.2019.d.ts',
-      '**/lib.2020.d.ts',
-      '**/lib.2021.d.ts',
-      '**/lib.2022.d.ts',
-      '**/lib.2023.d.ts',
-      '**/lib.2024.d.ts',
-      '**/lib.2025.d.ts',
+      '**/lib.es2015.d.ts',
+      '**/lib.es2016.d.ts',
+      '**/lib.es2017.d.ts',
+      '**/lib.es2018.d.ts',
+      '**/lib.es2019.d.ts',
+      '**/lib.es2020.d.ts',
+      '**/lib.es2021.d.ts',
+      '**/lib.es2022.d.ts',
+      '**/lib.es2023.d.ts',
+      '**/lib.es2024.d.ts',
+      '**/lib.es2025.d.ts',
     ],
   })
   console.log(`[${toolName}] Number of type files: ${typeFiles.length}`)
