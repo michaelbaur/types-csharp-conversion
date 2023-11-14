f783a2f18b642aa4338ca9e1d3b7920e3ecb47bb
Johannes Rössel <github@hypftier.de>
2023-11-15 00:18:28 +0100

    Include all reserved keywords and don't append suffixes for clashes.

--- src/converter.ts
+++ src/converter.ts
@@ -11,22 +11,85 @@ type Context = {
   elementName?: string
 }
 
-const reservedNames: Record<string, boolean> = {
-  as: true,
-  base: true,
-  checked: true,
-  default: true,
-  event: true,
-  float: true,
-  is: true,
-  object: true,
-  operator: true,
-  public: true,
-  switch: true,
-  static: true,
-  private: true,
-  this: true,
-}
+const reservedNames: Set<string> = new Set([
+  'abstract',
+  'as',
+  'base',
+  'bool',
+  'break',
+  'byte',
+  'case',
+  'catch',
+  'char',
+  'checked',
+  'class',
+  'const',
+  'continue',
+  'decimal',
+  'default',
+  'delegate',
+  'do',
+  'double',
+  'else',
+  'enum',
+  'event',
+  'explicit',
+  'extern',
+  'false',
+  'finally',
+  'fixed',
+  'float',
+  'for',
+  'foreach',
+  'goto',
+  'if',
+  'implicit',
+  'in',
+  'int',
+  'interface',
+  'internal',
+  'is',
+  'lock',
+  'long',
+  'namespace',
+  'new',
+  'null',
+  'object',
+  'operator',
+  'out',
+  'override',
+  'params',
+  'private',
+  'protected',
+  'public',
+  'readonly',
+  'ref',
+  'return',
+  'sbyte',
+  'sealed',
+  'short',
+  'sizeof',
+  'stackalloc',
+  'static',
+  'string',
+  'struct',
+  'switch',
+  'this',
+  'throw',
+  'true',
+  'try',
+  'typeof',
+  'uint',
+  'ulong',
+  'unchecked',
+  'unsafe',
+  'ushort',
+  'using',
+  'virtual',
+  'void',
+  'volatile',
+  'while',
+])
 
 function getTypeName(type: ts.TypeNode, context: Context): string {
   switch (type.kind) {
@@ -119,13 +182,7 @@ function getSafeName(
     ? member.text
     : (member.name as ts.StringLiteral).text
   // TODO Improve writing of reserved member names
-  return !reservedNames[name]
-    ? name
-    : ts.isIdentifier(member)
-    ? `${name}Parameter`
-    : ts.isPropertySignature(member)
-    ? `${name}Property`
-    : `${name}Method`
+  return !reservedNames.has(name) ? name : `@${name}`
 }
 
 function isReadonly(modifiers: ts.NodeArray<ts.Modifier> | undefined): boolean {
